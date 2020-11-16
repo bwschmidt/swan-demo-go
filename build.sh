e36dfef23c0b930f71220c49a2c2eda2745e3306
Janko Ulaga <janko.ulaga@gmail.com>
2020-11-16 19:55:40 +0100

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Stops the process if something fails
+set -xe
+
+# create the application binary that eb uses
+GOOS=linux GOARCH=amd64 go build  -ldflags="-s -w" -o ./application ./src/server.go 
+
+zip aws-eb-swan-demo.zip application appsettings.json Procfile
