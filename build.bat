a69d27d75be0e0101be08b8d6abfe81b5a2e12ca
Joseph Dix <joseph@51degrees.com>
2020-11-23 14:02:01 +0000

    MINOR: Use REM in batch file comments

--- build.bat
+++ build.bat
@@ -1,15 +1,15 @@
-# This is needed because the windows zip process used by EB will not enable
-# the executable to be run on linux.
-# https://forums.aws.amazon.com/message.jspa?messageID=825738#825738
-# go.exe get -u github.com/aws/aws-lambda-go/cmd/build-lambda-zip
+REM This is needed because the windows zip process used by EB will not enable
+REM the executable to be run on linux.
+REM https://forums.aws.amazon.com/message.jspa?messageID=825738REM825738
+REM go.exe get -u github.com/aws/aws-lambda-go/cmd/build-lambda-zip
 
-# Set up Go for AWS Elastic Beanstalk (EB) build
+REM Set up Go for AWS Elastic Beanstalk (EB) build
 set GOPATH=%CD%
 set GOARCH=amd64
 set GOOS=linux
 
-# Build the application
+REM Build the application
 go build -o ./application ./src/server.go
 
-# Create a zip file with the application and the settings file
+REM Create a zip file with the application and the settings file
 bin\build-lambda-zip.exe -o aws-eb-swan-demo.zip application appsettings.json Procfile .ebextensions/.config images/190811762.jpeg images/221406343.jpeg images/234657570.jpeg
