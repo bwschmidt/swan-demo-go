3304288f55f97f0054b54075063ec8911e474911
James Rosewell <james@51degrees.com>
2021-01-28 10:30:12 +0000

    DOC/BUILD: Modified the build and readme to automatically get the AWS zip application. Also modifies the README to explain the "www" folder and structure.

--- build.ps1
+++ build.ps1
@@ -3,22 +3,33 @@
 # https://forums.aws.amazon.com/message.jspa?messageID=825738REM825738
 # go.exe get -u github.com/aws/aws-lambda-go/cmd/build-lambda-zip
 
-# Set up Go for AWS Elastic Beanstalk (EB) build
+# Set the common build environment variables.
 $Env:GOPATH=Get-Location
 $Env:GOARCH="amd64"
+
+# Set up the AWS zip file command for Windows if it does not exist.
+$zipcmd = "bin\build-lambda-zip.exe"
+if (!(Test-Path $zipcmd))
+{
+    $Env:GOOS="windows"
+    Invoke-Expression "go.exe get -u github.com/aws/aws-lambda-go/cmd/build-lambda-zip"
+}
+
+# Set up Go for AWS Elastic Beanstalk (EB) build
 $Env:GOOS="linux"
 
 # Build the application
 Invoke-Expression "go build -o ./application ./src/server.go"
 
 # Get all the files in the www folder that form the content.
-$files = Get-ChildItem -File -Path ./www -Recurse | Resolve-Path -Relative
+$www = Get-ChildItem -File -Path ./www -Recurse | Resolve-Path -Relative
 
 # Create the zip command with all the files.
-$command = "bin\build-lambda-zip.exe -o aws-eb-swan-demo.zip application appsettings.json Procfile .ebextensions/.config " + $www -join ' '
-$command = $command.Replace(".\", "").Replace("\", "/")
+if (Test-Path "application")
+{
+    $command = "bin\build-lambda-zip.exe -o aws-eb-swan-demo.zip application appsettings.json Procfile .ebextensions/.config " + $www -join ' '
+    $command = $command.Replace(".\", "").Replace("\", "/")
 
-# Create a zip file with the application and the settings file
-Invoke-Expression $command
-
-# bin\build-lambda-zip.exe -o aws-eb-swan-demo.zip application appsettings.json Procfile .ebextensions/.config images/190811762.jpeg images/221406343.jpeg images/234657570.jpeg
+    # Create a zip file with the application and the settings file
+    Invoke-Expression $command
+}
